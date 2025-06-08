fde97275c593b13ff4aa6c8e35b6b185d34e03f4
lyujeong-gyun <prank01@naver.com>
2025-06-08 21:18:55 +0900

    java-adv2

---
+++ src/network/exception/connect/ConnectMain.java
@@ -0,0 +1,31 @@
+package network.exception.connect;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ConnectMain {
+
+    public static void main(String[] args) throws IOException {
+        unknownHostEx1();
+        unknownHostEx2();
+    }
+
+    private static void unknownHostEx1() throws IOException {
+        try {
+            Socket socket = new Socket("999.999.999.999", 80);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private static void unknownHostEx2() throws IOException {
+        try {
+            Socket socket = new Socket("google.gogo", 80);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
--- src/network/tcp/autocloseable/ResourceV2.java
+++ src/network/tcp/autocloseable/ResourceV2.java
@@ -1,10 +1,10 @@
 package network.tcp.autocloseable;
 
-public class ResourceV1 {
+public class ResourceV2 implements AutoCloseable {
 
     private String name;
 
-    public ResourceV1(String name) {
+    public ResourceV2(String name) {
         this.name = name;
     }
 
@@ -17,13 +17,9 @@ public class ResourceV1 {
         throw new CallException(name + " ex");
     }
 
-    public void close() {
+    @Override
+    public void close() throws CloseException {
         System.out.println(name + " close");
-    }
-
-    //자원을 정리하는 도중에 예외가 터지는 경우
-    public void closeEx() throws CloseException {
-        System.out.println(name + " closeEx");
         throw new CloseException(name + " ex");
     }
 }
