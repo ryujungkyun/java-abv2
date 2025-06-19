8fe24c30e1b8645f9c1874bd96492e84ff3136e3
lyujeong-gyun <prank01@naver.com>
2025-06-20 03:37:15 +0900

    java-adv2

---
+++ src/annotation/TestController.java
@@ -0,0 +1,16 @@
+package annotation;
+
+public class TestController {
+
+    @SimpleMapping(value = "/")
+    public void home() {
+        System.out.println("TestController.home");
+    }
+
+    @SimpleMapping(value = "/site1")
+    public void page1() {
+        System.out.println("TestController.page1");
+    }
+
+
+}
